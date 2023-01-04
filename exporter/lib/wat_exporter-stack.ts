24ca572261a86b9223573c9a2cccb8543736bb89
Anton Kovalenko <antkoval@amazon.de>
2023-01-04 17:35:32 +0100

    Fixed handlers

--- exporter/lib/wat_exporter-stack.ts
+++ exporter/lib/wat_exporter-stack.ts
@@ -8,12 +8,13 @@ export class WatExporterStack extends cdk.Stack {
     super(scope, id, props);
 
       //Get list of the Well-Architected Tool workflows function and permissions to WAT
-      const code_wf_list = readFileSync('lambdas/wf_list/lambda.js', 'utf8')
+      // const code_wf_list = readFileSync('functions/getWorkflowList/index.js', 'utf8')
       const fn_wf_list = new aws_lambda.Function(this, 'WorkflowsListFunction', {
           functionName: 'WorkflowsListFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          code: aws_lambda.Code.fromInline(code_wf_list),
-          handler: 'handler'
+          // code: aws_lambda.Code.fromInline(code_wf_list),
+          code: aws_lambda.Code.fromAsset('functions/getWorkflowList'),
+          handler: 'index.handler'
       })
       fn_wf_list.addToRolePolicy(
           new aws_iam.PolicyStatement({
@@ -23,12 +24,13 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //Get Well Architected Tool workflow content function and permissions to WAT
-      const code_wf = readFileSync('lambdas/wf/lambda.js', 'utf8')
+      // const code_wf = readFileSync('functions/getWorkflow/index.js', 'utf8')
       const fn_wf = new aws_lambda.Function(this, 'WorkflowFunction', {
           functionName: 'WorkflowFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          code: aws_lambda.Code.fromInline(code_wf),
-          handler: 'handler'
+          // code: aws_lambda.Code.fromInline(code_wf),
+          code: aws_lambda.Code.fromAsset('functions/getWorkflow'),
+          handler: 'index.handler'
       })
       fn_wf.addToRolePolicy(
           new aws_iam.PolicyStatement({
@@ -38,12 +40,13 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //Onboard backend user function and permissions to put parameters to Parameter Store
-      const code_user_onboard = readFileSync('lambdas/onboard/lambda.js', 'utf8')
+      // const code_user_onboard = readFileSync('functions/onBoard/index.js', 'utf8')
       const fn_user_onboard = new aws_lambda.Function(this, 'UserOnboardFunction', {
           functionName: 'UserOnboardFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          code: aws_lambda.Code.fromInline(code_user_onboard),
-          handler: 'handler'
+          // code: aws_lambda.Code.fromInline(code_user_onboard),
+          code: aws_lambda.Code.fromAsset('functions/onBoard'),
+          handler: 'index.handler'
       })
       fn_user_onboard.addToRolePolicy(
           new aws_iam.PolicyStatement({
@@ -53,12 +56,13 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //API GW authorizer function and permissions to get parameters from Parameter Store
-      const code_apigw_auth = readFileSync('lambdas/apigw_auth/lambda.js', 'utf8')
+      // const code_apigw_auth = readFileSync('functions/authorize/index.js', 'utf8')
       const fn_apigw_auth = new aws_lambda.Function(this, 'APIGWauthFunction', {
           functionName: 'APIGWauthFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          code: aws_lambda.Code.fromInline(code_apigw_auth),
-          handler: 'handler'
+          // code: aws_lambda.Code.fromInline(code_apigw_auth),
+          code: aws_lambda.Code.fromAsset('functions/authorize'),
+          handler: 'index.handler'
       })
       fn_apigw_auth.addToRolePolicy(
           new aws_iam.PolicyStatement({
@@ -79,6 +83,6 @@ export class WatExporterStack extends cdk.Stack {
       rs_wf.addMethod('GET', new aws_apigateway.LambdaIntegration(fn_wf))
       //Resource and method for to onboard user
       const rs_user_onboard = api_gw.root.addResource('onboard')
-      rs_wf.addMethod('POST', new aws_apigateway.LambdaIntegration(fn_user_onboard))
+      rs_user_onboard.addMethod('POST', new aws_apigateway.LambdaIntegration(fn_user_onboard))
   }
 }
