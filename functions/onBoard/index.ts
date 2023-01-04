b534688a914d7020aee004497559ac74e5c80d3f
Egor Miasnikov <emiasnik@3c06304684e0.ant.amazon.com>
2023-01-04 19:11:42 +0100

    Added team id to param store

--- functions/onBoard/index.ts
+++ functions/onBoard/index.ts
@@ -7,18 +7,17 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
     const client = new SSMClient({ region });
     const requestBody = JSON.parse(event.body)
 
-    // const command = new PutParameterCommand({
-    //     Name: "", Value: ""
-    // });
-    // const response = await client.send(command);
+    const command = new PutParameterCommand({
+        Name: "miroTeamId", Value: requestBody.team
+    });
+    await client.send(command);
 
-    console.log(`requestBody: ${JSON.stringify(requestBody, null, 2)}`);
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            message: 'OK',
         }),
      };
   };
