a6db2a60d6ff31df913196790b7ab114c84584ee
Egor Miasnikov <emiasnik@3c06304684e0.ant.amazon.com>
2023-01-04 17:40:50 +0100

    Updated get workflow method

--- functions/getWorkflow/index.ts
+++ functions/getWorkflow/index.ts
@@ -1,12 +1,23 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import {WellArchitectedClient, GetWorkloadCommand, ListWorkloadsCommand} from "@aws-sdk/client-wellarchitected"
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    const region = "eu-north-1" // will get it from frontend
+    const WorkloadId = "51dbee7d651de15ab74e145620e1f34c"
+
+    const client = new WellArchitectedClient({ region });
+    const command = new GetWorkloadCommand({
+        WorkloadId
+    });
+    const response = await client.send(command);
+
+    console.log(`Response: ${JSON.stringify(response, null, 2)}`);
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            workflow: response,
         }),
      };
   };
