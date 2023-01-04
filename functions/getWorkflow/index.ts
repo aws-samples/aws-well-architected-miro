67ab7af29d48c0e3f4689ec5e5115c1ecc42eb3c
Egor Miasnikov <emiasnik@3c06304684e0.ant.amazon.com>
2023-01-04 18:01:53 +0100

    Added getting workload id from event params

--- functions/getWorkflow/index.ts
+++ functions/getWorkflow/index.ts
@@ -3,7 +3,7 @@ import { WellArchitectedClient, GetWorkloadCommand } from "@aws-sdk/client-wella
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     const region = "eu-north-1" // will get it from frontend
-    const WorkloadId = "51dbee7d651de15ab74e145620e1f34c"
+    const WorkloadId = event.pathParameters.workloadId
 
     const client = new WellArchitectedClient({ region });
     const command = new GetWorkloadCommand({
