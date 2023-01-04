6daa67def466386f93c71d4bd4e41c439272e3ad
Egor Miasnikov <emiasnik@3c06304684e0.ant.amazon.com>
2023-01-04 19:03:58 +0100

    Updated onboard function

--- functions/onBoard/index.ts
+++ functions/onBoard/index.ts
@@ -1,6 +1,18 @@
-import { Context, APIGatewayProxyResult, APIGatewayEvent, Callback } from 'aws-lambda';
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { SSMClient, PutParameterCommand } from "@aws-sdk/client-ssm";
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    // a client can be shared by different commands.
+    const region = 'eu-north-1'
+    const client = new SSMClient({ region });
+    const requestBody = JSON.parse(event.body)
+
+    // const command = new PutParameterCommand({
+    //     Name: "", Value: ""
+    // });
+    // const response = await client.send(command);
+
+    console.log(`requestBody: ${JSON.stringify(requestBody, null, 2)}`);
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
