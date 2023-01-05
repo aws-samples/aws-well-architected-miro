0ef7b18e6290140a907427828cbe3fb44e46f52a
Anton Kovalenko <antkoval@amazon.de>
2023-01-05 09:55:18 +0100

    Added region as path part for API GW

--- exporter/lib/wat_exporter-stack.ts
+++ exporter/lib/wat_exporter-stack.ts
@@ -1,18 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {aws_apigateway, aws_iam, aws_lambda} from "aws-cdk-lib";
-import {readFileSync} from "fs";
 
 export class WatExporterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
       //Get list of the Well-Architected Tool workflows function and permissions to WAT
-      // const code_wf_list = readFileSync('functions/getWorkflowList/index.js', 'utf8')
       const fn_wf_list = new aws_lambda.Function(this, 'WorkflowsListFunction', {
           functionName: 'WorkflowsListFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          // code: aws_lambda.Code.fromInline(code_wf_list),
           code: aws_lambda.Code.fromAsset('functions/getWorkflowList'),
           handler: 'index.handler'
       })
@@ -24,11 +21,9 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //Get Well Architected Tool workflow content function and permissions to WAT
-      // const code_wf = readFileSync('functions/getWorkflow/index.js', 'utf8')
       const fn_wf = new aws_lambda.Function(this, 'WorkflowFunction', {
           functionName: 'WorkflowFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          // code: aws_lambda.Code.fromInline(code_wf),
           code: aws_lambda.Code.fromAsset('functions/getWorkflow'),
           handler: 'index.handler'
       })
@@ -40,11 +35,9 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //Onboard backend user function and permissions to put parameters to Parameter Store
-      // const code_user_onboard = readFileSync('functions/onBoard/index.js', 'utf8')
       const fn_user_onboard = new aws_lambda.Function(this, 'UserOnboardFunction', {
           functionName: 'UserOnboardFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          // code: aws_lambda.Code.fromInline(code_user_onboard),
           code: aws_lambda.Code.fromAsset('functions/onBoard'),
           handler: 'index.handler'
       })
@@ -56,11 +49,9 @@ export class WatExporterStack extends cdk.Stack {
       )
 
       //API GW authorizer function and permissions to get parameters from Parameter Store
-      // const code_apigw_auth = readFileSync('functions/authorize/index.js', 'utf8')
       const fn_apigw_auth = new aws_lambda.Function(this, 'APIGWauthFunction', {
           functionName: 'APIGWauthFunction',
           runtime: aws_lambda.Runtime.NODEJS_16_X,
-          // code: aws_lambda.Code.fromInline(code_apigw_auth),
           code: aws_lambda.Code.fromAsset('functions/authorize'),
           handler: 'index.handler'
       })
@@ -75,14 +66,16 @@ export class WatExporterStack extends cdk.Stack {
       const api_gw = new aws_apigateway.RestApi(this, 'ApiGateway', {
           restApiName: "BackendGateway"
       })
+      //Create API GW root path with region
+      const rs_region = api_gw.root.addResource('{region}')
       //Resource and method to get workflows list from Well-Architected Tool
-      const rs_wf_list = api_gw.root.addResource('get_wf_list')
+      const rs_wf_list = rs_region.addResource('get_wl_list')
       rs_wf_list.addMethod('GET', new aws_apigateway.LambdaIntegration(fn_wf_list))
       //Resource and method to get workflow details
-      const rs_wf = api_gw.root.addResource('get_wf').addResource('{workloadId}')
+      const rs_wf = rs_region.addResource('get_wl').addResource('{workloadId}')
       rs_wf.addMethod('GET',new aws_apigateway.LambdaIntegration(fn_wf))
       //Resource and method to onboard user
-      const rs_user_onboard = api_gw.root.addResource('onboard')
+      const rs_user_onboard = rs_region.addResource('onboard')
       rs_user_onboard.addMethod('POST', new aws_apigateway.LambdaIntegration(fn_user_onboard))
   }
 }
