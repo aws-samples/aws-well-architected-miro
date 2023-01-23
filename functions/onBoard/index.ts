d94f3a8e56b819d1f48bd7f4787f9831ce1d6616
Egor Miasnikov <emiasnik@3c06304684e0.ant.amazon.com>
2023-01-23 15:19:42 +0100

    Improve onboarding lambda to overwrite param

--- functions/onBoard/index.ts
+++ functions/onBoard/index.ts
@@ -1,5 +1,5 @@
 import {APIGatewayEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
-import {ParameterType, PutParameterCommand, SSMClient} from "@aws-sdk/client-ssm";
+import {ParameterType, PutParameterCommand, GetParameterCommand, SSMClient} from "@aws-sdk/client-ssm";
 import {decode, Jwt, JwtPayload} from "jsonwebtoken";
 
 interface MiroJwtToken extends Jwt {
@@ -14,32 +14,41 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
     const region = event.pathParameters.region
     const client = new SSMClient({ region });
     const Name = 'miroTeamId'
-
-    console.log(`event.headers: ${JSON.stringify(event.headers, null, 2)}`);
-    console.log(`event.headers["Authorization"]: ${JSON.stringify(event.headers["Authorization"], null, 2)}`);
     const authorizationHeader = event.headers["Authorization"]
-    console.log(`authorizationHeader: ${JSON.stringify(authorizationHeader, null, 2)}`);
     const jwtToken: string = authorizationHeader.split(' ')[1];
 
     const jwsDecoded = decode(jwtToken, { complete: true, json: true}) as MiroJwtToken
     const miroTeamFromJwt = jwsDecoded?.payload.team
 
-    console.log(`miroTeamFromJwt: ${JSON.stringify(miroTeamFromJwt, null, 2)}`);
+    const getParameter = new GetParameterCommand({Name});
+    const storedParameter = await client.send(getParameter);
 
-    const command = new PutParameterCommand({
-        Name, Value: miroTeamFromJwt, Type: ParameterType.STRING
-    });
-    await client.send(command);
-
+    if(storedParameter.Parameter.Value === miroTeamFromJwt){
+        const putParameter = new PutParameterCommand({
+            Name, Value: miroTeamFromJwt, Type: ParameterType.STRING, Overwrite: true
+        });
+        await client.send(putParameter);
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify({
+                message: 'OK',
+            }),
+        };
+    }
     return {
-        statusCode: 200,
+        statusCode: 401,
         headers: {
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
         body: JSON.stringify({
-            message: 'OK',
+            message: 'Your team is not authorized to use this app.',
         }),
-     };
+    };
   };
