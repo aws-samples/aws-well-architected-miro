9aa0a192302a5c8aba278cebc75d82827789d880
Egor Miasnikov <emiasnik@amazon.com>
2023-03-16 19:03:53 +0100

    Improved connection to param store in onboarding

--- functions/onBoard/index.ts
+++ functions/onBoard/index.ts
@@ -11,6 +11,7 @@ interface MiroJwtTokenPayload extends JwtPayload {
     "user": string, //ID of the Miro user that the JWT is assigned to
 }
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    let storedParameter;
     const region = process.env.AWS_REGION
     const client = new SSMClient({ region });
     const Name = 'miroTeam'
@@ -20,23 +21,33 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
     const jwsDecoded = decode(jwtToken, { complete: true, json: true}) as MiroJwtToken
     const miroTeamFromJwt = jwsDecoded?.payload.team
 
-    const getParameter = new GetParameterCommand({Name});
-    const storedParameter = await client.send(getParameter);
+    try{
+        const getParameter = new GetParameterCommand({Name});
+        storedParameter = await client.send(getParameter);
+        if(storedParameter.Parameter.Value === miroTeamFromJwt){
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'OK',
+                }),
+            };
+        }
+        
+    }catch (e) {
+        if(e.name === 'ParameterNotFound'){
 
-    if(storedParameter.Parameter.Value === miroTeamFromJwt){
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'OK',
-            }),
-        };
-    }
+            const putParameter = new PutParameterCommand({
+                Name, Value: miroTeamFromJwt, Type: ParameterType.STRING, Overwrite: true
+            });
+            await client.send(putParameter);
 
-    if(!storedParameter){
-        const putParameter = new PutParameterCommand({
-            Name, Value: miroTeamFromJwt, Type: ParameterType.STRING, Overwrite: true
-        });
-        await client.send(putParameter);
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'OK',
+                }),
+            };
+        }
 
     }
 
